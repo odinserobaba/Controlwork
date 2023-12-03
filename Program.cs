deb77911645f1ad53e64bbd806ee8a14a6434c14
odinserobaba <odinserobaba@gmail.com>
2023-12-03 17:28:30 +0300

    Add FilterString

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string[] inputArray = Console.ReadLine().Split(' ');
+
+string[] FilterStrings(string[] inputArray)
+{
+    int count = 0;
+
+    // Подсчет количества строк, удовлетворяющих условию
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    // Создание нового массива
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    // Заполнение нового массива строками, удовлетворяющими условию
+    foreach (string item in inputArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[index] = item;
+            index++;
+        }
+    }
+
+    return resultArray;
+}
